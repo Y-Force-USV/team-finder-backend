f54b9aa2eb5abb50695bc2f71bccaf8037bcaf67
Sorin Lupastean <lupasteansorin1@gmail.com>
2024-03-14 20:43:55 +0200

    Updated SkillsModule with forwardRef for module dependencies.

--- src/modules/skills/skills.module.ts
+++ src/modules/skills/skills.module.ts
@@ -5,11 +5,18 @@ import { SkillsController } from './skills.controller';
 import { SkillsService } from './skills.service';
 import { DepartmentsModule } from '../departments/departments.module';
 import { UsersModule } from '../users/users.module';
+import { SkillCategory } from '../skill-category/skill-category.entity';
+import { SkillsCategoryModule } from '../skill-category/skills-category.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Skill]), DepartmentsModule],
+  imports: [
+    TypeOrmModule.forFeature([Skill]),
+    forwardRef(() => DepartmentsModule),
+    forwardRef(() => UsersModule),
+    forwardRef(() => SkillsCategoryModule),
+  ],
   controllers: [SkillsController],
   providers: [SkillsService],
-  exports: [TypeOrmModule.forFeature([Skill]), SkillsService],
+  exports: [TypeOrmModule.forFeature([Skill, SkillCategory]), SkillsService],
 })
 export class SkillsModule {}
