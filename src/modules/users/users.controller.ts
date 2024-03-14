4218b8828c756f79601964102d4f734502ba8c63
Sorin Lupastean <lupasteansorin1@gmail.com>
2024-03-14 02:17:37 +0200

    Implemented authentication and role-based access control in UsersController using UseGuards and HasRoles decorators

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,30 +1,39 @@
-import { Body, Controller, Get, Param, Patch, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateAdminAndOrgDto, CreateEmployeeDto } from './users.dtos';
 import { AssignSkillsDto } from '../skills/skills.dtos';
 import { UserRole } from './user.entity';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { HasRoles } from '../auth/decorators/roles.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @HasRoles(UserRole.EMPLOYEE)
   async createEmpolyee(@Body() data: CreateEmployeeDto) {
     return await this.usersService.createEmployee(data);
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   async findUsers() {
     return await this.usersService.findUsers();
   }
 
   @Post('admin')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @HasRoles(UserRole.ADMIN)
   async createAdminAndOrg(@Body() data: CreateAdminAndOrgDto) {
     return await this.usersService.createAdminAndOrg(data);
   }
 
   @Patch(':id/role')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @HasRoles(UserRole.ADMIN)
   async updateUserRole(@Param('id') userId: number, @Body('role') newRole: UserRole) {
     return this.usersService.updateUserRole(userId, newRole);
   }
